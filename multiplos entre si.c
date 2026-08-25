44c10786bbd7b9d387ef42031554bee45173ea52
GonzzagaDev <netopool0@gmail.com>
2026-08-25 09:23:13 -0300

    Create multiplos entre si.c

---
+++ multiplos entre si.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[]) {
+	int a,b,r;
+	printf("Entre com os valores do problema:\n");
+	scanf("%d %d", &a, &b);
+	
+	if(a>0 && b>0){
+		if(a%2){
+			if(b%2){
+				printf("Sao multiplo de 2");
+			}
+		}
+		if(a&b == 0){
+			if(a&b == 0){
+				printf("sao multiplos  entre si");
+			}
+		}
+		
+		
+		
+		
+	}else{
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
